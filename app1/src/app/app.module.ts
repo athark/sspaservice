a2b6ab3335773b8feac770fc6bb7575f7e2a2dc0
athark <58450866+athark@users.noreply.github.com>
2021-01-03 16:57:25 -0600

    service added

---
+++ app1/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { EmptyRouteComponent } from './empty-route/empty-route.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { PostsComponent } from './posts/posts.component';
+import { HttpClientModule } from "@angular/common/http";
+import { PostsService } from './services/posts.service';
+import { UsersService } from './services/users.service';
+import { UsersComponent } from './users/users.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmptyRouteComponent,
+    LoginComponent,
+    RegisterComponent,
+    PostsComponent,
+    UsersComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+  ],
+  providers: [PostsService,UsersService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
